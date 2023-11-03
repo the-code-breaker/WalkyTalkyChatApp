dc816a1ac6d338def2973a78405cc9792a379dfa
Sourav Sharma <souravsharma191919@gmail.com>
2023-11-03 13:21:05 +0530

    new changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
-      '/api' : "http://localhost:5000" 
-    }
+      "/api": {
+        target: "https://face-detect-server.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
-  plugins: [react()],
-})
+});
