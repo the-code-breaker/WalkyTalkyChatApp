0213c11c20ca678fb7be249aeb60100299354881
Sourav Sharma <souravsharma191919@gmail.com>
2023-11-05 00:42:28 +0530

    bgimage

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "https://walkytalky-r0fb.onrender.com",
     },
   },
   plugins: [react()],
