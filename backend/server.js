62d06ef00adcf948cba0e9117209ff73dac2525f
Sourav Sharma <souravsharma191919@gmail.com>
2023-11-05 12:12:30 +0530

    endpoint

--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://walkytalky-r0fb.onrender.com",
+    origin: "https://walkytalky-r0fb.onrender.com/",
     methods: ["GET", "POST"],
     credentials: true,
   },
