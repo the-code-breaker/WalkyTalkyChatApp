b6c6abe3f8fe0cd7412939c9935253dbeb3f23ae
Sourav Sharma <souravsharma191919@gmail.com>
2023-11-07 01:50:52 +0530

    socket io connection

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://walkytalky-r0fb.onrender.com:8080",
+      "/api": "http://localhost:8080",
     },
   },
   plugins: [react()],
